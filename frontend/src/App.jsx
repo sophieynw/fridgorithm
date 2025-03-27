747057873947e1f89bedccd5556ada87b666eed7
Nghi Lam Vo <lamvo.general@gmail.com>
2025-03-26 23:28:05 -0400

    Add toast notification for sign up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 // App.jsx
 import React from 'react';
+import './styles/toast.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Welcome from './components/Welcome';
 import Login from './components/Login';
-import SignUp from './components/SignUp'; 
+import SignUp from './components/SignUp';
 import ForgotPassword from './components/ForgotPassword';
 import MainPage from './components/MainPage';
 import About from './components/About';
@@ -11,20 +12,21 @@ import Contact from './components/Contact';
 import ImageAnalysisTest from './components/ImageAnalysisTest'; // Added this line for testing VisionAI
 
 const App = () => {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Welcome />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<SignUp />} />
-                <Route path="/forgotpassword" element={<ForgotPassword />} />
-                <Route path="/mainpage" element={<MainPage />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/contact" element={<Contact />} />
-                <Route path="/image-test" element={<ImageAnalysisTest />} /> {/* Added this line for testing VisionAI */}
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/forgotpassword" element={<ForgotPassword />} />
+        <Route path="/mainpage" element={<MainPage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/image-test" element={<ImageAnalysisTest />} />{' '}
+        {/* Added this line for testing VisionAI */}
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
